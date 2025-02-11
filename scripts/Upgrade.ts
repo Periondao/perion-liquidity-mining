45b94d51011e6bbdde84f6687350c949d6906628
bitcoinwarrior1 <j.l.sangalli@gmail.com>
2025-02-11 13:34:47 +1100

    with commented out sPERC vars

--- scripts/Upgrade.ts
+++ scripts/Upgrade.ts
@@ -3,7 +3,9 @@ import hre, { ethers } from "hardhat";
 import { ProxyAdmin__factory, TimeLockNonTransferablePoolV2__factory } from "../typechain";
 
 const proxyStakingContract = "0xc014286360Ef45aB15A6D3f6Bb1E54a03352aC8f";
+// sPERC: 0xf64F48A4E27bBC299273532B26c83662ef776b7e
 const proxyAdmin = "0x70c20550c4a481c8d044415ca0604bb5bebfc2fb";
+// sPERC: 0x4c295daFEf00456c58F48122A80249E6EcF2B9f5
 
 async function main() {
   const signer = (await ethers.getSigners())[0];
